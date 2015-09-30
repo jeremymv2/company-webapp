2b3a4a21a1f9accf26797d7d2aa78db8bf66e0bc
Jeremy Miller <jeremymv2@gmail.com>
2015-09-30 18:44:00 -0400

    Update default.rb

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,24 +7,28 @@
 # All rights reserved - Do Not Redistribute
 #
 #
+# install git client
 include_recipe 'git'
+# install nginx server
 include_recipe 'nginx'
 
+# create the landing directory
 directory node['company-webapp']['docroot'] do
   recursive true
   action :create
 end
 
+# grab the code, the current commit hash is used for idempotency
 git node['company-webapp']['docroot'] do
    repository node['company-webapp']['gitrepo']
    revision node['company-webapp']['gitbranch']
    action :sync
 end
 
+# override the default website with our own
 begin
   t = resources("template[#{node['nginx']['dir']}/sites-available/default]")
   t.source 'default-site.erb'
-  #t.cookbook #{cookbook_name}
   t.cookbook 'company-webapp'
   t.notifies :reload, 'service[nginx]', :delayed
 rescue Chef::Exceptions::ResourceNotFound
